042891de3ae2e326caebf325aea92451cf2f3e89
Urvi Bhanushali <ujb2@illinois.edu>
2022-11-09 17:40:36 -0600

    added 2 insert apis

--- dbconn.py
+++ dbconn.py
@@ -1,43 +1,6 @@
-# from flask import Flask, request, render_template
-# import pymysql
-# 
-# db = pymysql.connect(host="localhost", user="root", password="", database="")
-# 
-# app = Flask(__name__)
-# api = dbconn(app)
-# 
-# @app.route('/')
-# def someName():
-#     cursor = db.cursor()
-#     sql = "SELECT * FROM apartment"
-#     cursor.execute(sql)
-#     results = cursor.fetchall()
-#     return render_template('index.html', results=results)
-# 
-# if __name__ == '__main__':
-# 	app.run(debug=True)
-
-# from flask import Flask
-# from flaskext.mysql import MySQL
-# 
-# app = Flask(__name__)
-# mysql = MySQL()
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = ''
-# app.config['MYSQL_DATABASE_DB'] = 'plus'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
-# conn = mysql.connect()
-# cursor =conn.cursor()
-# 
-# cursor.execute("SELECT * from apartment")
-# data = cursor.fetchone()
-# print(data)
-
-from flask import Flask
+from flask import Flask, request, jsonify
 import mysql.connector 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+
 app = Flask(__name__)
 
 # cnx = mysql.connector.connect(user='root', password='',
@@ -48,8 +11,6 @@ cnx = mysql.connector.connect(user='root', password='plus1234',
                               host='104.154.107.248',
                               database='plus')
                               
-                              
-
 try:
    cursor = cnx.cursor()
    cursor.execute("""
@@ -77,14 +38,54 @@ def hello_world():
 # ‘/’ URL is bound with hello_world() function.
 def apt():
     return result1
+
+@app.route('/addUnit', methods=['POST'])
+def add_unit():
+    _json = request.json
+    _unitnumber = _json['unitNumber']
+    _apartmentid = _json['apartmentId']
+    _area = _json['area']
+    _numbedrooms = _json['numBedrooms']
+    _rentalcost = _json['rentalCost']
+
+	# save edits
+    sql = "INSERT INTO unit(unitnumber, apartmentid, area, numbedrooms, rentalcost, availablity) VALUES(%s, %s, %s, %s, %s, %s)"
+    data = (_unitnumber, _apartmentid, _area, _numbedrooms, _rentalcost, 'yes')
+    try:	
+        cursor = cnx.cursor()
+        cursor.execute(sql, data)
+        cnx.commit()
+
+        resp = jsonify('User added successfully!')
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
  
+@app.route('/updateUnitCost', methods=['POST'])
+def update_unit_cost():
+    _json = request.json
+    _apartmentid = _json['apartmentId']
+    _numbedrooms = _json['numBedrooms']
+    _rentalcost = _json['rentalCost']
+
+	# save edits
+    sql = "UPDATE unit SET rentalcost = %s WHERE apartmentid = %s AND numbedrooms = %s AND availablity = 'yes'"
+    data = (_rentalcost, _apartmentid, _numbedrooms)
+    try:	
+        cursor = cnx.cursor()
+        cursor.execute(sql, data)
+        cnx.commit()
+
+        resp = jsonify('Rental cost updated successfully!')
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+
 # main driver function
 if __name__ == '__main__':
  
-    # run() method of Flask class runs the application
+    #  run() method of Flask class runs the application
     # on the local development server.
     app.run()
-
-   
-
-
